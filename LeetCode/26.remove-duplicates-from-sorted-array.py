97348a0398de9479f6f3c5c4888b02373d4547d4
mason45ok <128038735+mason45ok@users.noreply.github.com>
2024-03-05 10:34:30 +0800

    Update 26.remove-duplicates-from-sorted-array.py

--- LeetCode/26.remove-duplicates-from-sorted-array.py
+++ LeetCode/26.remove-duplicates-from-sorted-array.py
@@ -7,6 +7,19 @@
 # @lc code=start
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 1:
+            return n  # 如果陣列長度小於等於1，不需要處理重複元素
         
+        # 使用雙指針法，快指針用來遍歷整個陣列，慢指針用來表示去重後的陣列的尾部
+        slow = 0
+        
+        for fast in range(1, n):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+
+        return slow + 1  # 返回去除重複後的陣列的長度
+
 # @lc code=end
 
